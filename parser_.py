d8fecdf5ab494a117878a004a11256b7bdfd57f6
Daniel Escalera <101297050+Daniel-Ev-Esc@users.noreply.github.com>
2024-05-09 07:21:43 -0600

    Add files via upload

---
+++ parser_.py
@@ -0,0 +1,60 @@
+import ply.yacc as yacc
+
+from scanner import tokens
+
+def p_program(p):
+    'program : PROGRAM ID PUNTOCOMA vars_opt MAIN END'
+    p[0] = " ".join([p[1],p[2],p[3],p[4],p[5],p[6]])
+
+def p_vars_opt(p):
+    '''vars_opt : vars 
+    | empty'''
+    if(p[1]):
+        p[0] = p[1]
+    else:
+        p[0] = ""
+
+def p_vars(p):
+    'vars : VAR vars_1'
+    p[0] = " ".join([p[1],p[2]])
+
+def p_vars_1(p):
+    '''vars_1 : id DOSPUNTOS type PUNTOCOMA 
+    | empty'''
+    print(p[1])
+    if(p[1]):
+        p[0] = " ".join([p[1],p[2],p[3],p[4]])
+    else:
+        p[0] = ""
+
+def p_id(p):
+    'id : ID id_1'
+    p[0] = p[1]
+
+def p_id_1(p):
+    '''id_1 : COMA id
+    | empty'''
+    if(p[1]):
+        p[0] = " ".join([p[1],p[2]])
+    else:
+        p[0] = ""
+
+def p_type(p):
+    '''type : INT
+    | FLOAT'''
+    p[0] = p[1]
+
+def p_empty(p):
+    'empty : '
+    pass
+
+# Error rule for syntax errors
+def p_error(p):
+    print("Syntax error in input!")
+
+# Build the parser
+parser = yacc.yacc(start='program')
+
+s = ('program helloworld; var counter, indice :int; main end')
+result = parser.parse(s)
+print(result)
---
+++ scanner.py
@@ -0,0 +1,98 @@
+import ply
+import ply.lex as lex
+
+reserved = {
+    'program':'PROGRAM',
+    'main':'MAIN',
+    'end':'END',
+    'var':'VAR',
+    'int':'INT',
+    'float':'FLOAT',
+    'if' : 'IF',
+    'else' : 'ELSE',
+    'while' : 'WHILE',
+    'do':'DO',
+    'print':'PRINT',
+    'void':'VOID'
+}
+
+tokens = [
+    'PUNTOCOMA',
+    'COMA',
+    'DOSPUNTOS',
+    'CORCHETEIZQ',
+    'CORCHETEDER',
+    'IGUAL',
+    'PUNTO',
+    'PARENDER',
+    'PARENIZQ',
+    'MENORQUE',
+    'MAYORQUE',
+    'EXCLAMACION',
+    'PLUS',
+    'MINUS',
+    'MULT',
+    'DIV',
+    'BRACKETIZQ',
+    'BRACKETDER',
+    'COMILLAS',
+    'ID', 
+    'CTE_INT',
+    'CTE_FLOAT',
+    'CTE_STRING'  ] + list(reserved.values())
+
+t_PUNTOCOMA = r';'
+t_COMA = r','
+t_DOSPUNTOS= r':'
+t_CORCHETEIZQ= r'{'
+t_CORCHETEDER= r'}'
+t_IGUAL = r'='
+t_PUNTO = r'\.'
+t_PARENDER = r'\)'
+t_PARENIZQ = r'\('
+t_MENORQUE = r'<'
+t_MAYORQUE = r'>'
+t_EXCLAMACION = '¡'
+t_PLUS = r'\+'
+t_MINUS = r'- '
+t_MULT = r'\*'
+t_DIV = r'/'
+t_BRACKETIZQ = r'\['
+t_BRACKETDER = r'\]'
+t_COMILLAS = r'“'
+t_ignore  = ' \t'
+t_CTE_INT = r'-?[0-9]+'
+t_CTE_FLOAT = r'-?[0-9]+\.[0-9]+'
+t_CTE_STRING = r'\"[a-zA-Z_0-9 ]*\"'
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# Error handling rule
+def t_error(t):
+    raise Exception("Illegal character '%s'" % t.value[0])
+    
+
+lexer = lex.lex()
+
+# Test it out
+data = '''
+int i = 3 + 4 * 10;
+float = -9.8
+print("Esto es un string")
+'''
+
+# Give the lexer some input
+lexer.input(data)
+
+""" while True:
+    tok = lexer.token()
+    if not tok: 
+        break      # No more input
+    print(tok) """
